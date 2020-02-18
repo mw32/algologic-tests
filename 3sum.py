caca086e76c3e21e4d4576dd13774f862178972f
Martin Widjaja <martin.widjaja@workday.com>
2020-02-17 22:31:17 -0800

    3Sum from LeetCode Array/Strings Med/Hard https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/776/

---
+++ 3sum.py
@@ -0,0 +1,33 @@
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        ret = set()
+        p1, p2, p3 = 0, 1, len(nums) - 1
+        # print nums
+        # print p1, p2, p3
+        while (p1 < len(nums)-2):
+            i = -nums[p1]
+            if (nums[p3] + nums[p3-1]) < i:
+                p1 += 1
+                p2 += 1
+                # print "CONTINUE"
+                continue
+            while (p2 < p3):
+                # print p1, p2, p3
+                if (nums[p2] + nums[p3]) < i:
+                    p2 += 1
+                elif (nums[p2] + nums[p3]) > i:
+                    p3 -= 1
+                else:
+                    ret.add((nums[p1],nums[p2],nums[p3]))
+                    p2 += 1
+                    p3 -= 1
+                    # print ret
+            p1 += 1
+            p2 = p1 + 1
+            p3 = len(nums) - 1
+        return list(list(i) for i in ret)
+    
